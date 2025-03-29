edf6fff24e05e71bd49960fa838b6a71363805e6
Bishal Adhikari <82859040+Bishaladhikari2000@users.noreply.github.com>
2025-03-29 11:40:25 +0545

    Changes

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shoply/util/constants/sizes.dart';
+
+class TSpacingStyle {
+  static const EdgeInsetsGeometry paddingInAppBarHeight = EdgeInsets.only(
+    top: TSizes.appBarHeight,
+    left: TSizes.defaultSpace,
+    right: TSizes.defaultSpace,
+    bottom: TSizes.defaultSpace,
+  );
+}
